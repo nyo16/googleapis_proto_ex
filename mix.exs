8f97f4e7fdf56c6a7ba67d97b41d357ca4fd13b9
Niko Maroulis <nikos@streamlambda.com>
2020-04-04 22:26:58 -0400

    Lic added

--- mix.exs
+++ mix.exs
@@ -8,12 +8,30 @@ defmodule GoogleapisProtoEx.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
       name: "GoogleapisProtoEx",
+      package: package(),
       source_url: "https://github.com/nyo16/googleapis_proto_ex",
       docs: [
         main: "GoogleapisProtoEx",
         extras: ["README.md"]
       ]
+    ]
+  end
+
+  defp description() do
+    "Google protos for Bigtable, Pubsub and Datastore."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "googleapis_proto_ex",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/nyo16/googleapis_proto_ex"}
     ]
   end
 
